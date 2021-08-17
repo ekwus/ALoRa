9c25306b7d24d412e245a686555812ea8325a143
Jorge Miguel Miranda <jorge.miguel.miranda@gmail.com>
2021-08-17 15:09:42 +0100

    Remove unnecessary usings.

--- src/ALoRa.Library/TTNApplication.cs
+++ src/ALoRa.Library/TTNApplication.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
--- src/ALoRa.Library/TTNMessage.cs
+++ src/ALoRa.Library/TTNMessage.cs
@@ -1,9 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace ALoRa.Library
